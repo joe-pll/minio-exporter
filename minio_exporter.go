49cdb19607781ca506a292845ea1ee077be0ff7b
Giuseppe Pellegrino <mr.giuseppepellegrino@gmail.com>
2017-08-01 00:18:35 +0200

    FEATURE: added bucket statistics and environment variables

--- minio_exporter.go
+++ minio_exporter.go
@@ -7,12 +7,14 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
 
+	minio "github.com/minio/minio-go"
 	"github.com/minio/minio/pkg/madmin"
 )
 
@@ -40,11 +42,13 @@ var (
 // MinioExporter collects Minio statistics using the
 // Prometheus metrics package
 type MinioExporter struct {
-	Client *madmin.AdminClient
+	AdminClient *madmin.AdminClient
+	MinioClient *minio.Client
+	BucketStats bool
 }
 
 // NewMinioExporter inits and returns a MinioExporter
-func NewMinioExporter(uri string, minioKey string, minioSecret string) (*MinioExporter, error) {
+func NewMinioExporter(uri string, minioKey string, minioSecret string, bucketStats bool) (*MinioExporter, error) {
 	secure := false
 	newURI := uri
 
@@ -66,8 +70,15 @@ func NewMinioExporter(uri string, minioKey string, minioSecret string) (*MinioEx
 		return nil, fmt.Errorf("Minio admin client error %s", err)
 	}
 
+	minioClient, err := minio.New(urlMinio.Host, minioKey, minioSecret, secure)
+	if err != nil {
+		return nil, fmt.Errorf("Minio client error %s", err)
+	}
+
 	return &MinioExporter{
-		Client: mdmClient,
+		AdminClient: mdmClient,
+		MinioClient: minioClient,
+		BucketStats: bucketStats,
 	}, nil
 }
 
@@ -97,7 +108,7 @@ func (e *MinioExporter) Collect(ch chan<- prometheus.Metric) {
 }
 
 func execute(e *MinioExporter, ch chan<- prometheus.Metric) error {
-	status, err := e.Client.ServiceStatus()
+	status, err := e.AdminClient.ServiceStatus()
 	if err != nil {
 		return err
 	}
@@ -111,13 +122,16 @@ func execute(e *MinioExporter, ch chan<- prometheus.Metric) error {
 		prometheus.CounterValue,
 		status.Uptime.Seconds())
 
-	// Collect all server statistics
+	// Collect server admin statistics
 	collectServerStats(e, ch)
+	if e.BucketStats {
+		collectBucketStats(e, ch)
+	}
 	return nil
 }
 
 func collectServerStats(e *MinioExporter, ch chan<- prometheus.Metric) {
-	statsAll, _ := e.Client.ServerInfo()
+	statsAll, _ := e.AdminClient.ServerInfo()
 
 	for _, stats := range statsAll {
 		connStats := stats.Data.ConnStats
@@ -208,21 +222,104 @@ func collectStorageInfo(si madmin.StorageInfo, ch chan<- prometheus.Metric) {
 			nil),
 		prometheus.GaugeValue,
 		float64(si.Backend.Type), fstype)
+}
 
+func collectBucketStats(e *MinioExporter, ch chan<- prometheus.Metric) {
+	buckets, _ := e.MinioClient.ListBuckets()
+	wg := sync.WaitGroup{}
+	wg.Add(len(buckets))
+	for _, bucket := range buckets {
+		go func(bucket minio.BucketInfo, e *MinioExporter, ch chan<- prometheus.Metric) {
+			bucketStats(bucket, e, ch)
+			wg.Done()
+		}(bucket, e, ch)
+	}
+	wg.Wait()
+}
+
+func bucketStats(bucket minio.BucketInfo, e *MinioExporter, ch chan<- prometheus.Metric) {
+	location, _ := e.MinioClient.GetBucketLocation(bucket.Name)
+	var (
+		objNum               int64
+		bucketSize           int64
+		maxObjectSize        int64
+		incompleteUploads    int64
+		incompleteUploadSize int64
+	)
+	for objStat := range e.MinioClient.ListObjects(bucket.Name, "", true, nil) {
+		objNum = objNum + 1
+		bucketSize = bucketSize + objStat.Size
+		if objStat.Size > maxObjectSize {
+			maxObjectSize = objStat.Size
+		}
+	}
+
+	for upload := range e.MinioClient.ListIncompleteUploads(bucket.Name, "", true, nil) {
+		incompleteUploads = incompleteUploads + 1
+		incompleteUploadSize = incompleteUploadSize + upload.Size
+	}
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "bucket", "objects_number"),
+			"The number of objects in to the bucket",
+			[]string{"bucket", "location"},
+			nil),
+		prometheus.GaugeValue,
+		float64(objNum), bucket.Name, location)
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "bucket", "objects_total_size"),
+			"The total size of all object in to the bucket",
+			[]string{"bucket", "location"},
+			nil),
+		prometheus.GaugeValue,
+		float64(bucketSize), bucket.Name, location)
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "bucket", "max_object_size"),
+			"The maximum object size per bucket",
+			[]string{"bucket", "location"},
+			nil),
+		prometheus.GaugeValue,
+		float64(maxObjectSize), bucket.Name, location)
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "bucket", "incomplete_uploads_number"),
+			"The total number of incomplete uploads per bucket",
+			[]string{"bucket", "location"},
+			nil),
+		prometheus.GaugeValue,
+		float64(incompleteUploads), bucket.Name, location)
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "bucket", "incomplete_uploads_total_size"),
+			"The total size of incomplete upload per bucket",
+			[]string{"bucket", "location"},
+			nil),
+		prometheus.GaugeValue,
+		float64(incompleteUploadSize), bucket.Name, location)
 }
 
 func init() {
 	prometheus.MustRegister(version.NewCollector(program))
 }
 
+func getEnv(key string, defaultVal string) string {
+	if env, ok := os.LookupEnv(key); ok {
+		return env
+	}
+	return defaultVal
+}
+
 func main() {
 	var (
 		printVersion  = flag.Bool("version", false, "Print version information.")
-		listenAddress = flag.String("web.listen-address", ":9290", "Address to listen on for web interface and telemetry.")
-		metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
-		minioURI      = flag.String("minio.server", "http://localhost:9000", "HTTP address of the Minio server")
-		minioKey      = flag.String("minio.access-key", "", "The access key used to login in to Minio.")
-		minioSecret   = flag.String("minio.access-secret", "", "The access secret used to login in to Minio")
+		listenAddress = flag.String("web.listen-address", getEnv("LISTEN_ADDRESS", ":9290"), "Address to listen on for web interface and telemetry.")
+		metricsPath   = flag.String("web.telemetry-path", getEnv("MINIO_METRIC_PATH", "/metrics"), "Path under which to expose metrics.")
+		minioURI      = flag.String("minio.server", getEnv("MINIO_URL", "http://localhost:9000"), "HTTP address of the Minio server")
+		minioKey      = flag.String("minio.access-key", getEnv("MINIO_ACCESS_KEY", ""), "The access key used to login in to Minio.")
+		minioSecret   = flag.String("minio.access-secret", getEnv("MINIO_ACCESS_SECRET", ""), "The access secret used to login in to Minio")
+		bucketStats   = flag.Bool("minio.bucket-stats", false, "Collect bucket statistics. It can take long.")
 	)
 
 	flag.Parse()
@@ -232,7 +329,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	exporter, err := NewMinioExporter(*minioURI, *minioKey, *minioSecret)
+	exporter, err := NewMinioExporter(*minioURI, *minioKey, *minioSecret, *bucketStats)
 	if err != nil {
 		log.Fatalln(err)
 	}
